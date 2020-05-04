0b3b287c1fef5196e7c2f546ff497daed1f8787d
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-04 16:14:22 -0700

    map state to props and connect store in App.js
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Keturah Howard <keturah.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import EventDetails from './Components/EventDetails';
 import NewEventForm from './Components/NewEventForm';
+import {connect} from 'react-redux';
 
 function App(props) {
+  
+  // const [currentPage, setPage] = useState('MainPage');
+  // const [selectedEvent, setEvent] = useState({});
 
-  const [currentPage, setPage] = useState('MainPage');
-  const [selectedEvent, setEvent] = useState({});
+  //TODO:
+
+  //add handler functions, enact reducers {dispach(action)}
+  //pass preset data into event list through: app -> main -> eventList
+  //add firebase
+  //...
+  //profit
   
   function setVisibility() {
     if(currentPage === 'MainPage') {
-      return {component: <MainPage handleChangeViewClick={setPage} handleSelectEvent={setEvent} />}
+      return {component: <MainPage 
+        handleChangeViewClick={setPage}
+        handleSelectEvent={setEvent} />}
     } else if (currentPage === 'NewEventForm'){
       return {component: <NewEventForm handleBackToMainPage={setPage} />}
     } else if (currentPage === "EventDetails") {
@@ -23,13 +34,19 @@ function App(props) {
   const currentlyVisible = setVisibility();
   return (
     <React.Fragment>
-    <Header />
-    {currentlyVisible.component}
-    <Footer />
+      <Header />
+      {currentlyVisible.component}
+      <Footer />
     </React.Fragment>
   )
 }
+const mapStateToProps = state => ({
+  events: state.events,
+  presetEvents: state.presetEvents,
+  currentPage: state.currentPage,
+  selectedEvent: state.selectedEvent
+});
 
-
+App = connect(mapStateToProps)(App);
 
 export default App;
