dd7395781a9bddfa05cfe3bcbb39346fe828005a
Keturah Howard <keturah.dev@gmail.com>
2020-05-05 16:12:55 -0700

    Remove id from event to attemopt to bipass bug Co-authored-by: Steph Podolak <StephPodolak@gmail.com> Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com> Co-authored-by: Zakkrey <zakkreyjshort@gmail.com> Co-authored-by: S. Podolak <stephpodolak@gmail.com>

--- src/reducers/events-reducer.js
+++ src/reducers/events-reducer.js
@@ -1,13 +1,48 @@
-export default (state = {}, action) => {
-  const { title, host, date, id, yes, maybe, no } = action;
-  switch(action.type) {
-    case 'ADD_EVENT':
-      return {...state, [id]: {title, host, date, id, yes, maybe, no}};
-    case 'DELETE_EVENT':
-      const newState = { ...state };
-      delete newState[id];
-      return newState;
-    default:
-      return state;
-  }
-};
+// export default (state = {}, action) => {
+//   const { title, host, date, yes, maybe, no, col } = action;
+//   switch(action.type) {
+//     case 'ADD_EVENT':
+//       return {...state, [id]: {title, host, date,  yes, maybe, no}};
+//     case 'DELETE_EVENT':
+//       const newState = { ...state };
+//       delete newState[id];
+//       return newState;
+//     case 'INCREMENT_VOTE':
+//       if(action.col === 'yes') {
+//         const newState = { ...state, [id]: {
+//           title,
+//           host,
+//           date,
+//           // id,
+//           yes: yes++,
+//           maybe,
+//           no 
+//         }};
+//         return newState;
+//       } else if(action.col === 'maybe') {
+//         const newState = { ...state, [id]: {
+//           title,
+//           host,
+//           date,
+//           // id,
+//           yes,
+//           maybe: maybe++,
+//           no 
+//         }};
+//         return newState;
+//       } else if(action.col === 'no') {
+//         const newState = { ...state, [id]: {
+//           title,
+//           host,
+//           date,
+//           // id,
+//           yes,
+//           maybe,
+//           no: no++ 
+//         }};
+//         return newState;
+//       }
+//     default:
+//       return state;
+//   }
+// };
--- src/reducers/selected-event-reducer.js
+++ src/reducers/selected-event-reducer.js
@@ -1,8 +1,8 @@
 export default (state = {}, action) => {
   switch(action.type) {
     case "SELECT_EVENT":
-      const {title, host, date, id, yes, maybe, no} = action;
-      return {title, host, date, id, yes, maybe, no};
+      const {title, host, date,  yes, maybe, no} = action;
+      return {title, host, date,  yes, maybe, no};
     default:
       return state;
   }
