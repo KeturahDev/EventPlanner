99be3eae6a8cff50eeaf7a1c76bf0c14e85a3bdc
Keturah Howard <keturah.dev@gmail.com>
2020-05-05 09:54:50 -0700

    update reducer to incorporate ids
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Zakkrey <zakkreyjshort@gmail.com>

--- src/reducers/selected-event-reducer.js
+++ src/reducers/selected-event-reducer.js
@@ -1,8 +1,8 @@
 export default (state = {}, action) => {
   switch(action.type) {
     case "SELECT_EVENT":
-      const {title, host, date} = action;
-      return {title, host, date};
+      const {title, host, date, id} = action;
+      return {title, host, date, id};
     default:
       return state;
   }
