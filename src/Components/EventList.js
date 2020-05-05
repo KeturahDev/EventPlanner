150cdf0b1e0b9e926d4df2c7bf84c0c37bd5e4ac
Keturah Howard <keturah.dev@gmail.com>
2020-05-05 10:31:39 -0700

    adding keys to mapped child components
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>

--- src/Components/EventList.js
+++ src/Components/EventList.js
@@ -2,18 +2,20 @@ import React from 'react';
 import Event from './Event';
 import Events from "./../Data/Events";
 
+
 function EventList(props) {
     return (
         <React.Fragment>
         <div style={{border: "2px solid purple"}}>
             <p>here's a list of events</p>
-             {Object.values(Events).map((event) => 
+             {Object.values(Events).map((event, index) => 
               <Event 
                 title={event.title}
                 host={event.host}
                 date={event.date} 
                 handleChangeViewClick={props.handleChangeViewClick}
                 handleSelectEvent={props.handleSelectEvent}
+                key={index}
                />
              )}
         </div>
