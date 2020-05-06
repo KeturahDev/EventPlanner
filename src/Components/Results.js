c495f36b16ee533e7f27641161e5563d914b7314
Keturah Howard <keturah.dev@gmail.com>
2020-05-06 11:37:08 -0700

    add data visualization with Victory

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,19 +1,37 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import { VictoryBar, VictoryChart } from 'victory';
 
 function Results(props){
 
-  // form - takes button info...
+  const [data, notSure] = useState({ fill: "tomato" })
+  const [clicked, clickClack] = useState(false)
+
+  const dataArr = [ 
+    {status: "yes", votes: props.yescount},
+    {status: "maybe", votes: props.maybecount},
+    {status: "no", votes: props.nocount} 
+  ]
 
   return(
     <React.Fragment>
       <div style={{border: "2px solid blue"}}>
         <p>throw graph/data analytics here</p>
-
+        {/* {graph} */}
         {props.yescount}
         {props.maybecount}
         {props.nocount}
       </div>
+      <VictoryChart>
+        <VictoryBar 
+          data={dataArr}
+          // data accessor for x values
+          x="status"
+          // data accessor for y values
+          y="votes"
+          />
+      </VictoryChart>
     </React.Fragment>
   );
 }
