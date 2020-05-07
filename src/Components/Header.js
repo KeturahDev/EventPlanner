cddc1de57160c21db349fe456e47843961759314
Keturah Howard <keturah.dev@gmail.com>
2020-05-07 11:35:39 -0700

    make first styled component
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
+const EventPlannerHeader = styled.h1`
+  font-size: 50px;
+  text-align: center;
+  color: #b5f587;
+  background-image: linear-gradient(to bottom, #b3f484, #83b462, #567842, #2e4124, #060f00);
+  font-family: "Trebuchet MS", Helvetica, sans-serif;
+  margin:0%
+`;
 
 function Header(props){
   return (
     <React.Fragment>
-      <h1>Event Planner</h1>
+      <EventPlannerHeader>
+          Event Planner
+      </EventPlannerHeader>
+      {/* <h1>Event Planner</h1> */}
       <ul>
         <li onClick={props.handleClickLink}>
           <Link to="/">Home</Link>
