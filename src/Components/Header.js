85615f7840a0ed24e4676817663a72910d83b7b5
Keturah Howard <keturah.dev@gmail.com>
2020-05-07 21:26:24 -0700

    add navbar styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,27 +4,37 @@ import styled from 'styled-components';
 
 const EventPlannerHeader = styled.h1`
   font-size: 50px;
-  text-align: center;
+  ${'' /* text-align: center; */}
   color: #b5f587;
-  background-image: linear-gradient(to bottom, #b3f484, #83b462, #567842, #2e4124, #060f00);
+  ${'' /* background-image: linear-gradient(to bottom, #b3f484, #83b462, #567842, #2e4124, #060f00); */}
   font-family: "Trebuchet MS", Helvetica, sans-serif;
   margin:0%
 `;
 
+const notALink = {
+  textDecoration: "none",
+  color: "#b5f587",
+  fontSize: "25px",
+  backgroundColor: "#030c0185",
+  borderRadius: "9px",
+  padding: "5px"
+}
+
 function Header(props){
   return (
     <React.Fragment>
-      <EventPlannerHeader>
+      <ul className="centerBox" >
+      <EventPlannerHeader className="blocks" >
           Event Planner
       </EventPlannerHeader>
-      {/* <h1>Event Planner</h1> */}
-      <ul>
-        <li onClick={props.handleClickLink}>
-          <Link to="/">Home</Link>
+      <div className="linkBox">
+        <li className="blocks" onClick={props.handleClickLink}>
+          <Link style={notALink} to="/">Home</Link>
         </li>
-        <li onClick={props.handleClickLink}>
-          <Link to="/signin">Sign In</Link>
+        <li className="blocks" onClick={props.handleClickLink}>
+          <Link style={notALink} to="/signin">Sign In</Link>
         </li>
+      </div>
       </ul>
     </React.Fragment>
   );
