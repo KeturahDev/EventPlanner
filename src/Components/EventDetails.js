4d433d2960e0b167b76fed2b9c4d96abd8378e3d
Keturah Howard <keturah.dev@gmail.com>
2020-06-04 12:20:37 -0700

    style data view

--- src/Components/EventDetails.js
+++ src/Components/EventDetails.js
@@ -4,6 +4,15 @@ import Results from "./Results";
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux'
 import { useFirestoreConnect, isLoaded } from 'react-redux-firebase'
+import style from 'styled-components'
+
+const DetailBox = style.div `
+  border: 2px solid grey;
+  border-radius: 9px;
+  background-color: #909f86a7;
+  padding-left: 6px;
+  margin-top: 6px;
+`;
 
 function EventDetails(props) { //button id = prop?
   const {selectedEvent} = props;
@@ -19,16 +28,16 @@ function EventDetails(props) { //button id = prop?
     console.log("DB SELECTED:",dbSelectedEvent);
     return (
       <React.Fragment>
-        <div style={{ border: "2px solid orange" }}>
+        <DetailBox>
           <h3>{selectedEvent.title}</h3>
           <h4>On {dbSelectedEvent.date} by: {selectedEvent.host}</h4>
-          <RSVPForm selectedEvent={dbSelectedEvent} id={selectedEvent.id}/> {/* take an id as well to insert response into correct event? */}
-          
+
+          <RSVPForm selectedEvent={dbSelectedEvent} id={selectedEvent.id}/>
+          <hr/>
           <Results yescount={dbSelectedEvent.yes} maybecount={dbSelectedEvent.maybe} nocount={dbSelectedEvent.no}/>
           
-          <button onClick={() => props.handleBackToMainPage('MainPage')}>MainPage</button>
-          <hr />
-        </div>
+          <button className="button" onClick={() => props.handleBackToMainPage('MainPage')}>MainPage</button>
+        </DetailBox>
       </React.Fragment>
     );
   }
--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { VictoryBar, VictoryChart, VictoryTheme} from 'victory';
+import style from 'styled-components'
+
+const DataBox = style.div `
+  padding: 6px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Graph = style.div `
+  border-radius: 9px;
+  border: 2px solid grey;
+  margin: 10px;
+  background-color: rgba(253, 255, 253, 0.886);
+`;
+  
+const Answers = style.div `
+
+  margin: 10px;
+
+`;
 
 function Results(props){
 
@@ -12,24 +33,27 @@ function Results(props){
 
   return(
     <React.Fragment>
-      <div style={{border: "2px solid blue"}}>
-        <div className={"data"}>
-          <VictoryChart 
-          theme={VictoryTheme.material}
-          domainPadding={20}>
-            <VictoryBar 
-              data={dataArr}
-              style={{data: {fill: "pink"}}}
-              x="status"
-              y="votes"
-              />
-          </VictoryChart>
-        </div>
-      
-        <p>Going: {props.yescount}</p>
-        <p>Might be going: {props.maybecount}</p>
-        <p>Not going: {props.nocount}</p>
-      </div>
+      <DataBox>
+        <Graph>
+          <div className={"data"}>
+            <VictoryChart 
+            theme={VictoryTheme.material}
+            domainPadding={20}>
+              <VictoryBar 
+                data={dataArr}
+                style={{data: {fill: "pink"}}}
+                x="status"
+                y="votes"
+                />
+            </VictoryChart>
+          </div>
+        </Graph>
+        <Answers>
+          <p>Going: {props.yescount}</p>
+          <p>Might be going: {props.maybecount}</p>
+          <p>Not going: {props.nocount}</p>
+        </Answers>
+      </DataBox>
       
     </React.Fragment>
   );
