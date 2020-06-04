0f4da3cc68a6de912986e4008cfcd62daf0a196b
Keturah Howard <keturah.dev@gmail.com>
2020-06-04 13:09:13 -0700

    Updates

---
+++ precache-manifest.6fd96d2af462b5a397c922631e7de0f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3346d30ce489cddbdf959c7f0a27ecde",
+    "url": "/EventPlanner/index.html"
+  },
+  {
+    "revision": "035405b7de38304a27e1",
+    "url": "/EventPlanner/static/css/main.4bbf3dfe.chunk.css"
+  },
+  {
+    "revision": "b8bd0ecd2e1e74a46a9c",
+    "url": "/EventPlanner/static/js/2.58292c64.chunk.js"
+  },
+  {
+    "revision": "f9416c2fa5dace694260608407c8cf2e",
+    "url": "/EventPlanner/static/js/2.58292c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035405b7de38304a27e1",
+    "url": "/EventPlanner/static/js/main.7bee8b64.chunk.js"
+  },
+  {
+    "revision": "5447ef72b4314c287bc3",
+    "url": "/EventPlanner/static/js/runtime-main.d710f938.js"
+  }
+]);
